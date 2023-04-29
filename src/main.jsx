27ac8ef77d9bd851aa6bd0efddb1956f9dcc8775
Kumar Piyush <kr.piyush888@gmail.com>
2023-04-30 03:12:19 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import BrowserRouter from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename = {process.env.PUBLIC_URL}>
     <App />
-    </BrowserRouter>
   </React.StrictMode>,
 )
